2dfd9f41451d8695ca5f10ebb38cd6df6b006208
Egan Bisma <brickkace@gmail.com>
2022-12-28 23:13:14 +0700

    Initial Commit

---
+++ NFCHabits/NFCHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCHabitsApp.swift
+//  NFCHabits
+//
+//  Created by Egan Bisma on 12/28/22.
+//
+
+import SwiftUI
+
+@main
+struct NFCHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
